b4a84bc705d5ff234a3e8bb42a75ac8666bc3613
Robert Snyder <r.snyder49@gmail.com>
2018-08-02 02:17:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
 def starts_with_a_vowel?(word)
-
+  array = word.scan(/\b[aeiouAEIOU]\w*/)
+  if array === []
+    return false 
+  else
+  return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  array = text.scan(/\b[A-Z]\w*[.!?,;:]/)
+  if array === []
+    return false
+  else 
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
-
+  !phone.scan(/\b\d{10}\b/)
 end
