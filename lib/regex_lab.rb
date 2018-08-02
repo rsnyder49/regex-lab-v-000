77b35078d8301dc22d422f6cf309bc779b381e96
Robert Snyder <r.snyder49@gmail.com>
2018-08-02 02:29:23 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,5 +25,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !phone.scan(/\b\d{10}\d/)
+  !phone.scan(/(\d{3})*[-()]*\d{3}*[-()]*\d{4}/)
 end
